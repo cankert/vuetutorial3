c4ec51de97406347a8e10ad2fb1efd98c0e4b5b7
cankert <ankert.c@gmail.com>
2018-12-21 10:09:39 +0100

    Third

---
+++ src/Blogs.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="blogs">
+    <h2>{{ blogTitle }}</h2>
+    <button @click="changeTitle">Change Title</button>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Blogs",
+  data() {
+    return {
+      blogTitle: "Blogs",
+      posts: []
+    };
+  },
+  methods: {
+    changeTitle() {
+      this.blogTitle = "Amazing";
+    }
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        console.log(response);
+        this.posts = response.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+</script>
+
+<style>
+</style>
+
+
